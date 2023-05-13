9b9bb555aad937611f5ed0292fca63ba7c352a8c
Ryan Linnit <ryan@ryanl.co.uk>
2023-05-13 23:03:01 +0100

    update root post `latest_reply_date`

--- src/twok/database/crud/table/post.py
+++ src/twok/database/crud/table/post.py
@@ -11,3 +11,17 @@ class Post(Table):
         super().__init__(session)
 
         self.main_column = PostModel.title
+
+    def create(self, filter=None, **kwargs: dict):
+        db_post = super().create(filter, **kwargs)
+
+        if db_post:
+            self.update(self.root_parent(db_post), latest_reply_date=db_post.date)
+
+        return db_post
+
+    def root_parent(self, post: PostModel):
+        if post.parent:
+            return self.root_parent(post.parent)
+
+        return post
